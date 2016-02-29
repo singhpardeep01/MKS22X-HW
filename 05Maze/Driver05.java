cedb6e2aeb4528c95df9166b30e5151661303885
singhpardeep01 <psingh@stuy.edu>
2016-02-29 00:11:14 -0500

    starting maze

---
+++ 05Maze/Driver05.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+public class Driver05{
+
+    public static void main(String[]args) throws FileNotFoundException {
+        Maze f;
+        f = new Maze("data1.dat",true);//true animates the maze.
+        
+        f.clearTerminal();
+        f.solve();
+
+        f.clearTerminal();
+        System.out.println(f);
+    }
+}
