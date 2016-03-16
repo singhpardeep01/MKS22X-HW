4ea71cefb7c643e8e963920cbb2a4a59b0fb96d2
singhpardeep01 <psingh@stuy.edu>
2016-03-15 23:25:10 -0400

    get() add() set() for MyLinkedList

--- 08MyLinkedList/MyLinkedList.java
+++ 08MyLinkedList/MyLinkedList.java
@@ -22,19 +22,75 @@ public class MyLinkedList {
 
     private LNode start;
     private int size;
+    private LNode used;
 
     public MyLinkedList( int length ) {
-	start = null;
-	size = 0;
+	start = new LNode(0);
+	size = length;
+	used = start;
 	LNode current = start;
 	while ( length > 0 ) {
-	    current.setNext(LNode(0));
+	    current.setNext(new LNode(0));
 	    current = current.getNext();
+	    length--;
 	}
     }
     public boolean add( int value ) {
-	if ( size == 0 ) {
-	    start = new LNode(value);
+	used.setValue(value);
+	used = used.getNext();
+	return true;
+    }
+    public String toString() {
+	String retStr = "[";
+	LNode current = start;
+	retStr += current.getValue();
+	int c = 1;   
+	while ( size > c ) {
+	    current = current.getNext();
+	    retStr += ", " + current.getValue();
+	    c++;
 	}
-	else {
-L
+	retStr += "]";
+	return retStr;
+    }
+    public int get( int index ) {
+	LNode current = start;
+	int c = 0;   
+	while ( index > c ) {
+	    current = current.getNext();
+	    c++;
+	}
+	return current.getValue();
+    }
+    public int set( int index, int NewValue ) {
+	LNode current = start;
+	int c = 0;   
+	while ( index > c ) {
+	    current = current.getNext();
+	    c++;
+	}
+	int retInt = current.getValue();
+	current.setValue(NewValue);
+	return retInt;
+    }
+	
+    public static void main( String[] args ) {
+	MyLinkedList x = new MyLinkedList(5);
+	x.add(1);
+	x.add(2);
+	x.add(3);
+	x.add(4);
+	x.add(5);
+	System.out.println(x);
+	System.out.println( x.get(0) );
+	System.out.println( x.get(1) );
+	System.out.println( x.get(2) );
+	System.out.println( x.get(3) );
+	System.out.println( x.get(4) );
+	System.out.println( x.set(0,0) );
+	System.out.println( x.set(1,1) );
+	System.out.println( x.set(2,2) );
+	System.out.println( x.set(3,3) );
+	System.out.println( x.set(4,4) );
+    }    
+}
