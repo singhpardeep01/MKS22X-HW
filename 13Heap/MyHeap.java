76e0620631133d08910cc6a5760cf8207755127a
singhpardeep01 <singhpardeep01>
2016-05-10 13:09:17 -0400

    fixes on heap for min heap

--- 13Heap/MyHeap.java
+++ 13Heap/MyHeap.java
@@ -4,26 +4,45 @@ public class MyHeap<T extends Comparable<T>>
 {
     private int size;
     private T[] data;
-
+    private int max;
     public MyHeap() {
+	max = 1;
 	size = 0;
 	data = (T[]) new Comparable[10];
     }
     public MyHeap(T[] array) {
+	max = 1;
 	size = array.length;
 	data = (T[]) new Comparable[size+1];
 	for (int x = 1; x < data.length; x++) {
 	    data[x] = array[x-1];
 	}
+	//	System.out.println(this);
 	heapify();
     }
+    public MyHeap(boolean isMax) {
+	max = -1;
+	size = 0;
+	data = (T[]) new Comparable[10];
+    }
+    public MyHeap(T[] array, boolean isMax) {
+	max = -1;
+	size = array.length;
+	data = (T[]) new Comparable[size+1];
+	for (int x = 1; x < data.length; x++) {
+	    data[x] = array[x-1];
+	}
+	//	System.out.println(this);
+	heapify();
+    }
+    
     private void pushDown(int k) {
 	while (k*2 <= size && k*2+1 <= size) {
 	    //System.out.println(this);
 	    //System.out.println( k + " " + k*2 + " " + k*2+1);
 	    //System.out.println( data[k] + " " + data[k*2] + " " +data[k*2+1]);
-	    if (data[k*2].compareTo(data[k]) > 0 || data[k*2+1].compareTo(data[k]) > 0) {
-		if (data[k*2].compareTo(data[k*2+1]) > 0) {
+	    if (data[k*2].compareTo(data[k])*max> 0 || data[k*2+1].compareTo(data[k])*max > 0) {
+		if (data[k*2].compareTo(data[k*2+1])*max > 0) {
 		    T temp = data[k];
 		    data[k] = data[k*2];
 		    data[k*2] = temp;
@@ -40,13 +59,13 @@ public class MyHeap<T extends Comparable<T>>
 		break;
 	    }
 	}
-	if (k*2 <= size && data[k*2].compareTo(data[k]) > 0) {
+	if (k*2 <= size && data[k*2].compareTo(data[k])*max > 0) {
 	    T temp = data[k];
 	    data[k] = data[k*2];
 	    data[k*2] = temp;
 	    k *= 2;
 	}
-	else if (k*2+1 <= size && data[k*2+1].compareTo(data[k]) > 0) {
+	else if (k*2+1 <= size && data[k*2+1].compareTo(data[k])*max > 0) {
 	    T temp = data[k];
 	    data[k] = data[k*2+1];
 	    data[k*2+1] = temp;
@@ -57,7 +76,7 @@ public class MyHeap<T extends Comparable<T>>
 	while (k > 1) {
 	    //System.out.println( "lol" + k + " " + k/2);
 	    //System.out.println( data[k] + " " + data[k/2]);
-	    if (data[k].compareTo(data[k/2]) > 0) {
+	    if (data[k].compareTo(data[k/2])*max > 0) {
 		T temp = data[k];
 		data[k] = data[k/2];
 		data[k/2] = temp;
@@ -119,19 +138,17 @@ public class MyHeap<T extends Comparable<T>>
     }
 
     //do this last
-    public MyHeap(boolean isMax) {}
-    public MyHeap(T[] array, boolean isMax) {}
-    
+
     public static void main(String[] args) {
-	MyHeap<Integer> a = new MyHeap<Integer>();
-	a.add(1);
-	a.add(2);
-	a.add(3);
-	a.add(4);
+	MyHeap<Integer> a = new MyHeap<Integer>(false);
 	a.add(5);
+	a.add(4);
+	a.add(3);
+	a.add(2);
+	a.add(1);
 	System.out.println(a);
 	Integer[] b = new Integer[]{1,2,3,4,5};
-	MyHeap<Integer> c = new MyHeap<Integer>(b);
+	MyHeap<Integer> c = new MyHeap<Integer>(b,false);
 	System.out.println(c);
 	c.delete();
 	c.add(6);
